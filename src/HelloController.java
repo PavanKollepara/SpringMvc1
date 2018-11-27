9b394459fedb31bcf9df2a2bcd44691b5bbd86cd
PavanKollepara <pavan.kollepara@ideabytes.com>
2018-11-27 19:05:27 +0530

    2

--- src/HelloController.java
+++ src/HelloController.java
@@ -3,6 +3,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloController implements Controller {
 
