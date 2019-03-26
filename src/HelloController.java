73b5885256a91876edd1742def1f73f85e8675d9
PavanKollepara <pavan.kollepara@ideabytes.com>
2019-03-26 17:18:23 +0530

    Testing3

--- src/HelloController.java
+++ src/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController implements Controller {
 	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		// TODO Auto-generated method stub
 sysout("Hello");
-sysout("GoodMorning");
+sysout("GoodMorning");sysout("GoodMorning");
 		return null;
 	}
 
